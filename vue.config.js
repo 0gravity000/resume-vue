c4a55b28555907a2355bae29f361ad334d151d59
0gravity000 <0gravit000@gmail.com>
2022-04-18 21:07:19 +0900

    サイトのtitleを変更、meta discriptionを追加

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  assetsDir: 'static'
+  assetsDir: 'static',
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title: "Resume",  //public\index.html <title><%= htmlWebpackPlugin.options.title %></title> に反映される
+    }
+  }
 })
